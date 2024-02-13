acea04d6b5896f55a6e31bc0bf94f6d77805300e
Lucas Riestra <Lucas.riestra94@gmail.com>
2024-02-13 02:49:37 +0100

    inicioCarta

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -5,7 +5,7 @@ import { EmpanadasArray } from '../../../Database/empanadas';
 import 'swiper/swiper-bundle.css';
 import "./menu.css"
 
-const Menu = () => {
+const Carta = () => {
     return (
         <div className='Foods'>
             <h1 className='CategoryTitle'>Empanadas</h1>
@@ -33,4 +33,4 @@ const Menu = () => {
     );
 };
 
-export default Menu;
+export default Carta;
